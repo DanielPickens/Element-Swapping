3db625d2bdada02deef808982045c7326a64b769
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-11 05:47:52 -0600

    Update elementswap.js

--- elementswap.js
+++ elementswap.js
@@ -1,7 +1,7 @@
 function findMinArray(arr, k) {
 let outputArr = [arr.length];
     if(arr[k]<arr[0]) {
-        for(let i=k;i>0;i--) {  /
+        for(let i=k;i>0;i--) {  
             let temp = arr[i-1];
             arr[i-1]=arr[i];
             arr[i]=temp;
